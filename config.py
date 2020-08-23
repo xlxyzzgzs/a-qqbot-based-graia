a65c150207ccc41de379237f12eb2f083de39f0f
SaltedFish <1010594446@qq.com>
2020-08-23 15:38:38 +0800

    移除 config.py 的跟踪

--- config.py
+++
@@ -1,8 +0,0 @@
-from graia.application.session import Session
-def connection_config():
-    return Session(
-        host='host:port',       #你mirai-api-http监听的地址以及端口
-        authKey='authToken',    #在mirai-api-http设置的authKey
-        account=123456789,      #要使用的对应的bot 的qq号
-        websocket=True          #记得在mirai-api-http里面设置开启websocket
-    )
