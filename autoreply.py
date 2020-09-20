805d06f289e2eb4cc0b7294f92a3d1a7c091e16e
咸鱼 <1010594446@qq.com>
2020-09-20 23:40:20 +0800

    添加了几行注释

--- autoreply.py
+++ autoreply.py
@@ -2,6 +2,12 @@ from typing import List
 from pydantic import BaseModel
 import json
 class QQButtonAutoReply(BaseModel):
+    '''
+    slot :作用暂时不明确
+    action_data :用户点击后 自动发送（回复）的内容
+    name :显示在消息中的蓝色的内容
+    action :作用暂时不明确
+    '''
     slot:int
     action_data:str
     name:str
@@ -25,7 +31,14 @@ class QQButtonAutoReply(BaseModel):
             'name':self.name,
             'action':self.action
         }
-def GenerateAutoReplyButton(prompt:str,title:str,button:List[QQButtonAutoReply],forward:bool=True,showSender:bool=True):
+def GenerateAutoReplyButton(prompt:str,title:str,button:List[QQButtonAutoReply],forward:bool=True,showSender:bool=True)->str:
+    '''
+    prompt :显示在预览（即在消息列表）中显示的内容
+    title :消息中的黑色字体的部分（不可点击）
+    button :消息中包含的可点击的按钮，类型为 List[QQButtonAutoReply]
+    forward :是否可转发（个人猜测）
+    showSender :是否显示发送者（个人猜测）
+    '''
     button_str=[]
     for b in button:
         button_str.append(b.to_dict())
